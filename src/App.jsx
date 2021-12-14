8c9d314e0e375abfa8eb824a7b0c9d7c4311a130
Paul Brubaker <pbrubak@gmail.com>
2021-12-13 23:56:57 -0800

    supabase methods added and working

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { getGuests, addGuest, deleteGuestById } from './services/guests';
+
 export default function App() {
   return <h1>Hello World</h1>;
 }
---
+++ src/services/guests.js
@@ -0,0 +1,17 @@
+import { client, parseData } from './client'
+
+export async function getGuests() {
+  const request = await client.from('guests').select().order('created_at')
+  return parseData(request)
+}
+
+export async function addGuest({ name, entry }) {
+  const request = await client.from('guests').insert({ name, entry })
+  return parseData(request)
+}
+
+export async function deleteGuestById(id) {
+  const request = await client.from('guests').delete().match({ id })
+  return parseData(request)
+}
+
